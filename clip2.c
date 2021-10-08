98fbf44b2920beb3594dd063d63870b40370b004
ildar-shaimordanov <ildar.shaimordanov@gmail.com>
2021-10-08 14:01:45 +0300

    more accurate memory allocation

--- clip2.c
+++ clip2.c
@@ -2,9 +2,21 @@
 /*
  * Windows clipboard I/O implementation
  *
- * Some ideas were taken from win-clipboard
+ * Initially it was implemented by @sindresorhus (Sindre Sorhus) as two
+ * separate C-tools: "copy.c" to copy input to clipboard and "paste.c"
+ * to paste from clipboard. Later it was totaly rewritten as a Rust
+ * tool. This tool is only implemented on C and acts as a bi-directional
+ * pipe tool: capture output of a previous command in pipe and copy data
+ * to clipboard or forward clipboard content to another command in pipe
+ * or simply display it.
+ *
+ * See for details:
  * https://github.com/sindresorhus/win-clipboard/tree/v0.2.0
  * MIT License
+ *
+ * Look closer if it can be adapted for BusyBox:
+ * https://github.com/rmyorston/busybox-w32
+ * Licensed under GPLv2 or later, see file LICENSE in this source tree.
  *
  * Copyright 2021 Ildar Shaimordanov
  */
@@ -35,7 +47,20 @@ enum {
 };
 
 
+void warn_no_memory_and_exit(long size) {
+	fprintf(stderr, "Not enough memory for %d bytes\n", size);
+	exit(1);
+}
+
+
+void warn_help_and_exit(int code) {
+	fprintf(stderr, HELP);
+	exit(code);
+}
+
+
 #define CHUNK_SIZE	0x10000
+#define READ_SIZE	(CHUNK_SIZE >> 6)
 
 
 typedef struct chunk_tag {
@@ -47,9 +72,8 @@ typedef struct chunk_tag {
 
 void* chunkalloc() {
 	chunk *buf = malloc(sizeof(chunk));
-	if ( buf == NULL ) {
-		fprintf(stderr, "Memory allocation error\n");
-		exit(1);
+	if ( ! buf ) {
+		warn_no_memory_and_exit(sizeof(chunk));
 	}
 	buf->capacity = 0;
 	buf->next = NULL;
@@ -65,10 +89,10 @@ int setclip(int conv_mode, int cb_format) {
 
 	DWORD cbSize = 0;
 	DWORD readCount;
-	char buffer[CHUNK_SIZE >> 1];
+	DWORD copyCount = 0;
+	char buffer[READ_SIZE];
 
 	while ( readCount = fread(buffer, 1, sizeof(buffer), stdin) ) {
-		DWORD copyCount = 0;
 		for (DWORD i = 0; i < readCount; i++) {
 			char ch = buffer[i];
 			if ( ch == '\r' && conv_mode != CM_AS_IS ) {
@@ -80,11 +104,25 @@ int setclip(int conv_mode, int cb_format) {
 			head->buffer[copyCount++] = ch;
 		}
 
-		cbSize += copyCount;
-		head->capacity = copyCount;
+		/*
+		We have to be sure that when we've got and parsed the
+		next portion of input data, we won't cross the edge of
+		the current chunk. When we are asked to read UNIX-like
+		EOLs ("\n") and convert them to DOS-like EOLs ("\r\n")
+		the buffer grows. So we have to keep the double size of
+		the read buffer for emergency. For example, we could
+		be asked to read #READ_SIZE bytes of "\n" and convert
+		them to "\r\n".
+		*/
+		if ( copyCount >= CHUNK_SIZE - READ_SIZE * 2 ) {
+			cbSize += copyCount;
+			head->capacity = copyCount;
 
-		head->next = chunkalloc();
-		head = head->next;
+			copyCount = 0;
+
+			head->next = chunkalloc();
+			head = head->next;
+		}
 	}
 
 	if ( ferror(stdin) ) {
@@ -92,18 +130,30 @@ int setclip(int conv_mode, int cb_format) {
 		exit(1);
 	}
 
-	HANDLE hData = GlobalAlloc(GMEM_MOVEABLE, cbSize + sizeof(wchar_t));
-	LPBYTE pData = GlobalLock(hData);
+	/*
+	The last portion was read, parsed and stored in the last chuck,
+	but information wasn't updated in the variables properly while
+	looping. We do it here out of the loop.
+	*/
+	cbSize += copyCount + sizeof(wchar_t);
+	head->capacity = copyCount;
 
-	cbSize = 0;
+	HANDLE hData = GlobalAlloc(GMEM_MOVEABLE, cbSize);
+	if ( ! hData ) {
+		warn_no_memory_and_exit(cbSize);
+	}
+
+	char *pData = GlobalLock(hData);
+
 	head = chunks;
-	while ( head->capacity != 0 ) {
-		CopyMemory(pData + cbSize, head->buffer, head->capacity);
-		cbSize += head->capacity;
+	while ( head ) {
+		CopyMemory(pData, head->buffer, head->capacity);
+		pData += head->capacity;
 		chunk *prev = head;
 		head = head->next;
 		free(prev);
 	}
+	*(wchar_t *)pData = 0;
 
 	GlobalUnlock(hData);
 
@@ -116,19 +166,19 @@ int setclip(int conv_mode, int cb_format) {
 }
 
 
-int getclip(int conv_mode, int cb_format) {
+void getclip(int conv_mode, int cb_format) {
 	_setmode(_fileno(stdout), _O_BINARY);
 
 	OpenClipboard(NULL);
 
 	HANDLE hData = GetClipboardData(cb_format);
 	if ( ! hData ) {
-		return 0;
+		goto FINALLY;
 	}
 
 	char *pData = (char *)GlobalLock(hData);
 
-	char ch;
+	DWORD ch;
 	while ( ch = *pData++ ) {
 		if ( ch == '\r' && conv_mode != CM_AS_IS ) {
 			continue;
@@ -141,6 +191,7 @@ int getclip(int conv_mode, int cb_format) {
 
 	GlobalUnlock(hData);
 
+FINALLY:
 	CloseClipboard();
 }
 
@@ -149,24 +200,30 @@ int main(int argc, char **argv) {
 	int conv_mode = CM_AS_IS;
 	int cb_format = CF_UNICODETEXT;
 
-	for (int i = 0; i < argc; i++) {
-		char *arg = argv[i];
-		if ( strcmp(arg, "-h") == 0 || strcmp(arg, "--help") == 0 ) {
-			printf(HELP);
-			return 0;
-		}
-		if ( strcmp(arg, "-u") == 0 ) {
+	int ch;
+	while ( ( ch = getopt(argc, argv, "udUTh") ) != -1 ) {
+		switch (ch) {
+		case 'u':
 			conv_mode = CM_DOS2UNIX;
-		}
-		if ( strcmp(arg, "-d") == 0 ) {
+			break;
+		case 'd':
 			conv_mode = CM_UNIX2DOS;
-		}
-		if ( strcmp(arg, "-U") == 0 ) {
+			break;
+		case 'U':
 			cb_format = CF_UNICODETEXT;
-		}
-		if ( strcmp(arg, "-T") == 0 ) {
+			break;
+		case 'T':
 			cb_format = CF_TEXT;
+			break;
+		case 'h':
+			warn_help_and_exit(0);
+		default:
+			warn_help_and_exit(1);
 		}
+	}
+
+	if ( optind < argc ) {
+		warn_help_and_exit(1);
 	}
 
 	if ( ! _isatty(_fileno(stdin)) ) {
